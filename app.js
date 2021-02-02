7137293b8b411b50955e959c11369522715dc494
boodah96 <2014990065@ses.yu.edu.jo>
2021-02-02 15:10:47 +0200

    css

--- app.js
+++ app.js
@@ -1,83 +1,107 @@
 'use strict';
-var table=document.getElementById('table');
+var table = document.getElementById('table');
 
-function Car(carModel,modelYear,manufacturer){
-    this.carModel=carModel;
-    this.modelYear=modelYear;
-    this.manufacturer=manufacturer;
-    this.price=getRandomPrice();
+function Car(carModel, modelYear, manufacturer) {
+    this.carModel = carModel;
+    this.modelYear = modelYear;
+    this.manufacturer = manufacturer;
+    this.price = getRandomPrice();
     Car.prototype.allCar.push(this);
 }
 
-Car.prototype.allCar=[]; 
-Car.prototype.render=function(){
+Car.prototype.allCar = [];
+Car.prototype.render = function () {
     var tableRow = document.createElement('tr')
     table.appendChild(tableRow);
-    var carModel =document.createElement('td');
-    carModel.textContent=this.carModel;
+    var carModel = document.createElement('td');
+    carModel.textContent = this.carModel;
     tableRow.appendChild(carModel);
-    var modelYear =document.createElement('td');
-    modelYear.textContent=this.modelYear;
+    var modelYear = document.createElement('td');
+    modelYear.textContent = this.modelYear;
     tableRow.appendChild(modelYear);
-    var manufacturer =document.createElement('td');
-    manufacturer.textContent=this.manufacturer;
+    var manufacturer = document.createElement('td');
+    manufacturer.textContent = this.manufacturer;
     tableRow.appendChild(manufacturer);
-    var price =document.createElement('td');
-    price.textContent=this.price;
+    var price = document.createElement('td');
+    price.textContent = this.price;
     tableRow.appendChild(price);
 }
 
-if(localStorage.getItem('carData')){
-var allCar=JSON.parse(localStorage.getItem('carData'));
-for (var i=0;i<allCar.length;i++){
- new Car(allCar[i].carModel,allCar[i].modelYear,allCar[i].manufacturer);
-}
-creatTable();
+if (localStorage.getItem('carData')) {
+    var allCar = JSON.parse(localStorage.getItem('carData'));
+    for (var i = 0; i < allCar.length; i++) {
+        new Car(allCar[i].carModel, allCar[i].modelYear, allCar[i].manufacturer);
+    }
+    creatTable();
 }
 
-var addCar =document.getElementById('addCar');
-addCar.addEventListener('submit',newCar);
-function newCar(event){
+var addCar = document.getElementById('addCar');
+addCar.addEventListener('submit', newCar);
+function newCar(event) {
     event.preventDefault();
-var carModelData=  event.target.carModel.value;
-var modelYearData=  event.target.modelYear.value;
+    var carModelData = event.target.carModel.value;
+    var modelYearData = event.target.modelYear.value;
 
-var manufacturerData=  event.target.manufacturer.value;
-var newCar= new Car(carModelData,modelYearData,manufacturerData);
-creatTable();
-localStorage.setItem('carData',JSON.stringify(Car.prototype.allCar));
+    var manufacturerData = event.target.manufacturer.value;
+    var newCar = new Car(carModelData, modelYearData, manufacturerData);
+    creatTable();
+    localStorage.setItem('carData', JSON.stringify(Car.prototype.allCar));
 
 
 }
 
-function tableHeader(){
+function tableHeader() {
     var tableHead = document.createElement('tr')
     table.appendChild(tableHead);
-    var carModel =document.createElement('th');
-    carModel.textContent='Car model';
+    var carModel = document.createElement('th');
+    carModel.textContent = 'Car model';
     tableHead.appendChild(carModel);
-    var modelYear =document.createElement('th');
-    modelYear.textContent='Car model year';
+    var modelYear = document.createElement('th');
+    modelYear.textContent = 'Car model year';
     tableHead.appendChild(modelYear);
-    var manufacturer =document.createElement('th');
-    manufacturer.textContent='Car manufacturer';
+    var manufacturer = document.createElement('th');
+    manufacturer.textContent = 'Car manufacturer';
     tableHead.appendChild(manufacturer);
-    var price =document.createElement('th');
-    price.textContent='Car price';
+    var price = document.createElement('th');
+    price.textContent = 'Car price';
     tableHead.appendChild(price);
 }
- 
-function creatTable(){
-    table.innerHTML='';
+
+function creatTable() {
+    table.innerHTML = '';
     tableHeader();
-    for(var i=0;i<Car.prototype.allCar.length;i++){
-        var eachCar=Car.prototype.allCar[i];
+    for (var i = 0; i < Car.prototype.allCar.length; i++) {
+        var eachCar = Car.prototype.allCar[i];
         eachCar.render();
 
     }
+    var totalRow = document.createElement('tr');
+    table.appendChild(totalRow);
+    var carModel = document.createElement('th');
+    carModel.setAttribute('class','last')
+    carModel.textContent = '';
+    totalRow.appendChild(carModel);
+    var modelYear = document.createElement('th');
+    modelYear.setAttribute('class','last')
+    modelYear.textContent = 'Total price  ';
+    totalRow.appendChild(modelYear);
+    var manufacturer = document.createElement('th');
+    manufacturer.setAttribute('class','last')
+    manufacturer.textContent = '';
+    totalRow.appendChild(manufacturer);
+    var price = document.createElement('th');
+    var totalPrice = 0;
+    for (var i = 0; i < Car.prototype.allCar.length; i++) {
+        totalPrice = totalPrice + Car.prototype.allCar[i].price;
+
+
+    }
+    price.textContent = totalPrice;
+    totalRow.appendChild(price);
+
 }
 
 
-function getRandomPrice(){
-    return Math.floor(Math.random() * (100000 - 7000) ) + 7000;
+function getRandomPrice() {
+    return Math.floor(Math.random() * (100000 - 7000)) + 7000;
 }
